e4f798c32c60cf8841058033104c9945047a4736
MMaynor <mr.maynor20@gmail.com>
2026-02-04 16:33:13 +0700

    Coding challenge done

--- randnumguess.py
+++ randnumguess.py
@@ -4,6 +4,6 @@ randnum = random.randint(1,10)
 guess = int(input("Guess a number between 1-10: "))
 
 if guess == randnum:
-    print("Good Job! You guess correct!")
+    print("Good Job! You guessed correct!")
 else:
-    print("Try agian! The number was", randnum)
+    print("Try again! The number was", randnum, ".")
